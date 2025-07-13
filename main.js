482b63583e305ae60cc817022e51479138b6abc6
Kathi <katharinabornemann@gmail.com>
2025-07-13 15:41:27 +0200

    Updated geometry and its shading

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
 const canvas = document.getElementById('three-canvas');
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(45, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(canvas.clientWidth, canvas.clientHeight);
 
 // Create a cube
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff88, wireframe: true });
+const geometry = new THREE.TorusGeometry();
+
+// Add a light
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(5, 5, 5);
+scene.add(light);
+
+// Use a shaded material
+const material = new THREE.MeshStandardMaterial({
+  color: '#ff5733',
+  metalness: 0.5,
+  roughness: 0.1,
+});
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
+scene.add(ambientLight);
+
+
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 2;
+camera.position.z = 5;
 
 function animate() {
   requestAnimationFrame(animate);
